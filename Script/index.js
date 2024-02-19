4b506a7742794a9687bc5a9b95a921962964a273
MasumK09 <maasumkamal@gmail.com>
2024-02-19 16:35:33 +0600

    added js

---
+++ Script/index.js
@@ -0,0 +1,7 @@
+function busSeat(){
+    let busSeats = document.getElementById('a1')
+    let seatsLeft = document.getElementById('seats-left')
+    for (let index = 0; seatsLeft <= 0; index--) {
+        console.log(index) 
+    }
+}
